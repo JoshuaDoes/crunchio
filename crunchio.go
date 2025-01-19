b0cceb64328c43ca1e0e3572a4c451ae00bd6ade
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2025-01-18 20:24:23 -0500

    [REVERTME] Try to track streamable EOFs

--- crunchio.go
+++ crunchio.go
@@ -7,28 +7,31 @@ import (
 	crunch "github.com/superwhiskers/crunch/v3"
 )
 
-//Bytes requires a type to be able to represent itself as a byte slice
+// Bytes requires a type to be able to represent itself as a byte slice
 type Bytes interface {
 	Bytes() []byte
 }
 
 type Buffer struct {
 	*crunch.Buffer
+	toRead int64
 }
 
-func NewBuffer(slices ...[]byte) (*Buffer) {
-	return &Buffer{crunch.NewBuffer(slices...)}
+func NewBuffer(slices ...[]byte) *Buffer {
+	buf := new(Buffer)
+	buf.Buffer = crunch.NewBuffer(slices...)
+	return buf
 }
 
-//SetValue changes the data of the value using a supported input type
-//Data must be one of the following types:
-//- byte, bool, int, uint
-//- []byte
-//- string
-//- int16, int32, int64
-//- uint16, uint32, uint64
-//- float32, float64
-//Alternatively, data may satisfy the Bytes interface by providing a `Bytes() []byte` (such as another Buffer)
+// SetValue changes the data of the value using a supported input type
+// Data must be one of the following types:
+// - byte, bool, int, uint
+// - []byte
+// - string
+// - int16, int32, int64
+// - uint16, uint32, uint64
+// - float32, float64
+// Alternatively, data may satisfy the Bytes interface by providing a `Bytes() []byte` (such as another Buffer)
 func (buf *Buffer) WriteAbstract(data interface{}) error {
 	switch data.(type) {
 	case Bytes:
@@ -61,17 +64,18 @@ func (buf *Buffer) WriteAbstract(data interface{}) error {
 	return nil
 }
 
-//Write implements io.Writer
+// Write implements io.Writer
 func (buf *Buffer) Write(p []byte) (int, error) {
 	if p == nil || len(p) <= 0 {
 		return 0, nil
 	}
 	buf.Grow(int64(len(p)))
 	buf.WriteBytesNext(p)
+	buf.toRead += int64(len(p))
 	return len(p), nil
 }
 
-//Read implements io.Reader
+// Read implements io.Reader
 func (buf *Buffer) Read(p []byte) (int, error) {
 	if p == nil || len(p) <= 0 {
 		return 0, nil
@@ -87,16 +91,20 @@ func (buf *Buffer) Read(p []byte) (int, error) {
 		}
 		return len(bytes), nil
 	}
-	return 0, io.EOF
+	buf.toRead -= toRead
+	if buf.toRead <= 0 {
+		return 0, io.EOF
+	}
+	return 0, nil
 }
 
-//ReadByte implements io.ByteReader
+// ReadByte implements io.ByteReader
 func (buf *Buffer) ReadByte() (byte, error) {
 	b := buf.ReadByteNext()
 	return b, nil
 }
 
-//Seek implements io.Seeker
+// Seek implements io.Seeker
 func (buf *Buffer) Seek(offset int64, whence int) (int64, error) {
 	switch whence {
 	case io.SeekStart:
@@ -111,23 +119,23 @@ func (buf *Buffer) Seek(offset int64, whence int) (int64, error) {
 	return buf.ByteOffset(), nil
 }
 
-//Close implements io.Closer
+// Close implements io.Closer
 func (buf *Buffer) Close() error {
 	buf.Reset()
 	return nil
 }
 
-//Size returns the byte size of the buffer
+// Size returns the byte size of the buffer
 func (buf *Buffer) Size() int {
 	return int(buf.ByteCapacity())
 }
 
-//Reset wipes the buffer
+// Reset wipes the buffer
 func (buf *Buffer) Reset() {
 	buf.Reset()
 }
 
-//String returns a string representation of the buffer
+// String returns a string representation of the buffer
 func (buf *Buffer) String() string {
 	return string(buf.Bytes())
 }
